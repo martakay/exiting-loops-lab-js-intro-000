1a6d8b623c5e1c4a244ca5c5b40a36565483bca9
Marta Kandova <kmartaaaa1989@gmail.com>
2019-02-04 20:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function breakOut( array, changeValue, stopValue ) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === stopValue) {
+  for ( var i = 0; i < array.length; i++ ) {
+    if ( array[ i ] === stopValue ) {
       break
     }
-    array[i] = changeValue;
+    array[ i ] = changeValue;
   }
   return array;
 }
