758ad0f22a4cb741bafb44064300106ab9d18374
Marta Kandova <kmartaaaa1989@gmail.com>
2019-02-04 21:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function keepGoing( array, changeValue, skipValue ) {
 }
 
 function findBy( array, findFn ) {
-  for (var i = 0; i < array.length; i++) {
+  for ( var i = 0; i < array.length; i++ ) {
     if ( findFn( array[ i ] ) ) {
-      return true;
+      return array[i];
     }
     else {
       return null;
